de9e66d1308390592ce42396d8cb9a31b679a88e
kb8864 <yt7s858864@gmail.com>
2024-12-06 22:38:35 +0900

    型推論とany型と列挙型（enum）

---
+++ js/enum.js
@@ -0,0 +1,18 @@
+"use strict";
+var MouthName;
+(function (MouthName) {
+    MouthName[MouthName["January"] = 1] = "January";
+    MouthName[MouthName["February"] = 2] = "February";
+    MouthName[MouthName["March"] = 3] = "March";
+    MouthName[MouthName["April"] = 4] = "April";
+    MouthName[MouthName["May"] = 5] = "May";
+    MouthName[MouthName["June"] = 6] = "June";
+    MouthName[MouthName["July"] = 7] = "July";
+    MouthName[MouthName["August"] = 8] = "August";
+    MouthName[MouthName["September"] = 9] = "September";
+    MouthName[MouthName["October"] = 10] = "October";
+    MouthName[MouthName["November"] = 11] = "November";
+    MouthName[MouthName["December"] = 12] = "December";
+})(MouthName || (MouthName = {}));
+const month = MouthName.February;
+console.log(month);
