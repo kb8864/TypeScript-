c5e7d3ab0033cc76c95013c076b2e3d1854b4876
kb8864 <yt7s858864@gmail.com>
2024-12-07 11:08:13 +0900

    Bool値

---
+++ js/bool.js
@@ -0,0 +1,8 @@
+"use strict";
+const agree = true; //同意する場合
+if (agree) {
+    console.log('同意しました');
+}
+if (!agree) {
+    console.log('同意してください');
+}
---
+++ js/bool.ts
@@ -0,0 +1,9 @@
+const agree: boolean = true; //同意する場合
+
+if (agree) {
+  console.log('同意しました');
+}
+
+if (!agree) {
+  console.log('同意してください');
+}
